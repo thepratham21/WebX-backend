998cd4cfbccc7db1df5fc3e0990da67981c74a9b
Prathmesh Shinde <prathmeshshinde021@gmail.com>
2026-03-25 16:55:32 +0530

    Added: Implementd authentication routes and user model.

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import express from "express"
 import dotenv from "dotenv"
-import connectDB from "./src/config/db.js";
 dotenv.config()
+import connectDB from "./src/config/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+// Import routes
+import authRoutes from "./src/routes/authRoutes.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
+
+
+app.use("/api/auth", authRoutes)
+
+
+
 
 app.listen((PORT), () => {
     console.log("server running on port 3000");
