0ac41a5093b63873150f5b23d81f271d5c2d5ee3
Prathmesh Shinde <prathmeshshinde021@gmail.com>
2026-04-24 23:10:24 +0530

    Refactor: Update CORS configuration to use environment variable for frontend URL

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import connectDB from "./src/config/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
-// Import routes
 import authRoutes from "./src/routes/authRoutes.js"
 import userRoutes from "./src/routes/userRoutes.js"
 import websiteRoutes from "./src/routes/websiteRoutes.js"
@@ -18,22 +17,16 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }))
-
 
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
 app.use("/api/website", websiteRoutes)
 app.use("/api/payment", paymentRoutes)
 
-
-
-
-
-
 app.listen((PORT), () => {
-    console.log("server running on port 3000");
+    console.log(`server running on port ${PORT}`);
     connectDB()
 })
